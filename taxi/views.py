c7b5b29ae99f26264ccf331352b894aad5b0d77e
martoff <martoff@ukr.net>
2026-04-20 22:54:31 +0300

    'Solution'

--- taxi/views.py
+++ taxi/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
-from taxi.models import Driver, Car, Manufacturer
+from django.views import generic
+
+from .models import Manufacturer, Car, Driver
 
 
 def index(request):
@@ -13,3 +15,36 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+    template_name = "taxi/manufacturer_list.html"
+
+
+class CarListView(generic.ListView):
+    model = Car
+    paginate_by = 5
+    ordering = ["id"]
+    # Оптимізація N+1 для виробника
+    queryset = Car.objects.select_related("manufacturer")
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+    paginate_by = 5
+    ordering = ["id"]
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+    ordering = ["id"]
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    # Оптимізація N+1: підвантажуємо авто драйвера та виробників цих авто
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
